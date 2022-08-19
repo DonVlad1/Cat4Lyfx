d9a43fa82980897c3b585f8834d9553f9d97abca
zellski1234 <zellski1234@gmail.com>
2022-08-19 11:07:54 +0100

    removed unecessary code

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
 import styled from 'styled-components'
-import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
-import * as GiIcons from "react-icons/gi";
 import { Link } from 'react-router-dom';
-import { SidebarData } from './SidebarData'
+
 import "./Sidebar.css"
-import { IconContext} from 'react-icons'
+
 
 
 function Sidebar() {
@@ -33,23 +31,10 @@ function Sidebar() {
                     <li className='sidebar-toggle'>
                         <Link to="#" className='menu-bars'>
                             {/* changes x to white */}
-                            {/* <IconContext.Provider value={{color: '#fff'}}> */}
                                 {/* the X on the sidebar \/ */}
                                 <AiIcons.AiOutlineClose className='x-icon' onClick={showSidebar}/>
-                            {/* </IconContext.Provider> */}
                         </Link>
                     </li>
-                    {/* Below is the old navation information */}
-                    {/* {SidebarData.map((item, index) => {
-                        return (
-                            <li key={index} className={item.cName}>
-                                <Link to={item.path}>
-                                    {item.icon}
-                                    <span>{item.title}</span>
-                                </Link>
-                            </li>
-                            )
-                        })} */}
                         <div>
                             <button>Checkout</button>
                         </div>

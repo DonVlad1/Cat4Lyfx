3c3d8c48928f75e0cee53787b540a4ba5190b05d
Brett Garrett <b.t.garrett@aol.co.uk>
2022-08-19 18:59:13 +0100

    enddddd this

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -41,15 +41,6 @@ function Sidebar({ checkOut, setCheckOut })
                             <Link to="/checkout">
                                 <button className="sendCheckout">Checkout</button>
                             </Link>
-                            {checkOut.map((checkOutList, checkIndex) =>
-                            {
-                                console.log(checkOut)
-                                return (
-                                    <div>
-                                        <img key={checkIndex} src={checkOutList.catImage} />
-                                    </div>
-                                );
-                            })}
                         </div>
                     </ul>
                 </nav>

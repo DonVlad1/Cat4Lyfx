ea89b286ae98b9ea184b0c336832b03e107d7459
Vlad <vladut_stoian@live.co.uk>
2022-08-19 18:37:05 +0100

    Working memory storage through page mvoes

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -2,58 +2,60 @@ import React, { useState } from 'react';
 import styled from 'styled-components'
 import * as AiIcons from "react-icons/ai";
 import { Link } from 'react-router-dom';
+// import SideBarCheckout from './SideBarCheckout';
 
 import "./Sidebar.css"
 
 
 
-function Sidebar({checkOut, setCheckOut}) {
+function Sidebar({ checkOut, setCheckOut })
+{
     const [sidebar, setSidebar] = useState(false)
     const showSidebar = () => setSidebar(!sidebar)
 
 
     return (
-    <>  
+        <>
 
 
-        <div className={sidebar ? 'sideBar active sideBarOpen' : 'sideBar'}>
-            <Link to="#" className='menu-bars'>
-                <button className="basket" onClick={showSidebar}>
-                    <BasketDiv>
-                        Checkout Basket&nbsp;&nbsp;
-                        <AiIcons.AiOutlineShoppingCart className='cart-icon'/>
-                    </BasketDiv>
-                </button>
-            </Link>
-            <nav className='side-menu'>
-                <ul className='side-menu-items'>
-                    <li className='sidebar-toggle'>
-                        <Link to="#" className='menu-bars'>
-                            
-                        </Link>
-                    </li>
+            <div className={sidebar ? 'sideBar active sideBarOpen' : 'sideBar'}>
+                <Link to="#" className='menu-bars'>
+                    <button className="basket" onClick={showSidebar}>
+                        <BasketDiv>
+                            Checkout Basket&nbsp;&nbsp;
+                            <AiIcons.AiOutlineShoppingCart className='cart-icon' />
+                        </BasketDiv>
+                    </button>
+                </Link>
+                <nav className='side-menu'>
+                    <ul className='side-menu-items'>
+                        <li className='sidebar-toggle'>
+                            <Link to="#" className='menu-bars'>
+
+                            </Link>
+                        </li>
                         {/* new checkout button */}
                         <div>
                             <Link to="/checkout">
                                 <button>Checkout</button>
-                                <img className='image' src={checkOut.catImage} />
-                                <div>
-                                    {checkOut.map((checkOutList, checkIndex) =>
-                                    {
-                                        console.log(checkOut)
-                                        return (
-                                            <img key={checkIndex} src={checkOutList.catImage} />
-                                        );
-                                    })}
-                                </div>
                             </Link>
+                            {checkOut.map((checkOutList, checkIndex) =>
+                            {
+                                console.log(checkOut)
+                                return (
+                                    <div>
+                                        <img key={checkIndex} src={checkOutList.catImage} />
+                                    </div>
+                                );
+                            })}
                         </div>
-                </ul>
-            </nav>
-        </div>
-    </>
+                    </ul>
+                </nav>
+            </div>
+        </>
     )
 }
+
 
 export default Sidebar
 

a794eb92562051e673a96bef255141c1333750ef
Vlad <vladut_stoian@live.co.uk>
2022-08-20 23:09:12 +0100

    Corrected an alt-text

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components'
 import * as AiIcons from "react-icons/ai";
 import * as TbIcons from "react-icons/tb";
@@ -11,52 +11,57 @@ function Sidebar({ checkOut, setCheckOut, total, setTotal })
 {
     const [sidebar, setSidebar] = useState(false)
     const showSidebar = () => setSidebar(!sidebar)
-    useEffect(()=> {
+    useEffect(() =>
+    {
         // console.log(checkOut[0].catPrice)
         let sum = 0
-        for (let i = 0; i < checkOut.length; i++){
+        for (let i = 0; i < checkOut.length; i++)
+        {
             sum += Number(checkOut[i].catPrice)
         }
         setTotal(sum.toFixed(2))
-    }, [checkOut,setTotal])
+    }, [checkOut, setTotal])
 
-    function removeItem (index) {
-        for (let i = 0; i < checkOut.length; i++) {
-            if(checkOut[i].catId === index.catId){
+    function removeItem(index)
+    {
+        for (let i = 0; i < checkOut.length; i++)
+        {
+            if (checkOut[i].catId === index.catId)
+            {
                 let removeCatFromCart = [...checkOut];
                 removeCatFromCart.splice(i, 1)
                 setCheckOut(removeCatFromCart)
-            } 
-        }    
+            }
+        }
     }
-    
-            return (
+
+    return (
         <>
-        <div className={sidebar ? 'fixMeDaddy somethingElse' : 'fixMeDaddy'}>
-            <button className="basket" onClick={showSidebar}>
-                        <BasketDiv>
-                            Checkout Basket&nbsp;&nbsp;
-                            <AiIcons.AiOutlineShoppingCart className='cart-icon'/>
-                        </BasketDiv>
-            </button>
-        </div>
-        <div className={sidebar ? 'sideBar active' : 'sideBar'}>
-            <nav className='side-menu'>
-                <ul className='side-menu-items'>
-                    <li className='sidebar-toggle'>
-                    </li>
+            <div className={sidebar ? 'fixMeDaddy somethingElse' : 'fixMeDaddy'}>
+                <button className="basket" onClick={showSidebar}>
+                    <BasketDiv>
+                        Checkout Basket&nbsp;&nbsp;
+                        <AiIcons.AiOutlineShoppingCart className='cart-icon' />
+                    </BasketDiv>
+                </button>
+            </div>
+            <div className={sidebar ? 'sideBar active' : 'sideBar'}>
+                <nav className='side-menu'>
+                    <ul className='side-menu-items'>
+                        <li className='sidebar-toggle'>
+                        </li>
                         <div>
                             <div className="basketContents">
                                 {checkOut.map((checkOutList, index) =>
                                 {
                                     return (
                                         <div className="polaroidStyle" key={index}>
-                                            <img  src={checkOutList.catImage} className="basketImg"/>
+                                            <img src={checkOutList.catImage} className="basketImg" alt='checkOut' />
                                             <p className="caption">{checkOutList.catName}&nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp;£{checkOutList.catPrice}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                            <TbIcons.TbTrashX className='trashIcon' onClick={()=>removeItem(checkOutList, index)}/></p>
+                                                <TbIcons.TbTrashX className='trashIcon' onClick={() => removeItem(checkOutList, index)} /></p>
                                         </div>
-                                        );
-                                    })}
+                                    );
+                                })}
                             </div>
                             <p className="runningTotal">£{total}</p>
                             <Link to="/checkout">
